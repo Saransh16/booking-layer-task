94416f8f78def8b720717305a71c95145e74fea8
Saransh Kumar <ksaransh682@gmail.com>
2023-02-11 04:59:37 +0530

    Add in create rooms api

--- app/Http/Controllers/Api/RoomController.php
+++ app/Http/Controllers/Api/RoomController.php
@@ -2,18 +2,26 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Services\RoomService;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Room\CreateRequest;
 
 class RoomController extends Controller
 {
+    protected $service;
+
     public function __construct(RoomService $roomService)
     {
         $this->service = $roomService;
     }
 
-    public function create()
+    public function create(CreateRequest $request)
     {
+        $inputs = $request->validated();
 
+        $room = $this->roomService->create($inputs);
+
+        return response()->success($room);
     }
 }
--- app/Services/RoomService.php
+++ app/Services/RoomService.php
@@ -2,7 +2,22 @@
 
 namespace App\Services;
 
+use App\Interfaces\RoomRepositoryInterface;
+
 class RoomService
 {
+    protected $roomRepo;
+
+    public function __construct(RoomRepositoryInterface $roomRepo)
+    {
+        $this->roomRepo = $roomRepo;
+    }
+
+    public function create($inputs)
+    {
+        $room = $this->roomRepo->create($inputs);
+
+        return $room;
+    }
 
 }
