d9d9b4ec995e57725a31b0ff184804114b89a424
Saransh Kumar <ksaransh682@gmail.com>
2023-02-15 01:10:03 +0530

    Fix issue with the booking test failing after refresh database

--- tests/Feature/BookingTest.php
+++ tests/Feature/BookingTest.php
@@ -1,13 +1,25 @@
 <?php
 
+use App\Models\Room;
+use Illuminate\Foundation\Testing\WithFaker;
+
+uses(WithFaker::class);
+
+beforeEach(function () {
+
+    $this->room = Room::factory()->create([
+        'name' => $this->faker->word,
+        'capacity' => random_int(1, 10)
+    ]);
+});
+
 it('can create a booking', function () {
 
     $response = $this->post('/api/bookings', [
-        'room_id' => 2,
-        'starts_at' => "2023/02/11",
-        'ends_at' => "2023/02/13"
+        'room_id' => $this->room->id,
+        'starts_at' => date($format = 'Y/m/d'),
+        'ends_at' => date($format = 'Y/m/d')
     ]);
 
     $response->assertStatus(200);
 });
-
